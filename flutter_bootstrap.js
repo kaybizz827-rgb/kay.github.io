f1b54dac070bb0dc37016a07bbd972a6328249c6
kaybizz827-rgb <kaybizz827@gmail.com>
2025-12-16 23:31:27 +0000

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "377463313"
+    serviceWorkerVersion: "2309684529"
   }
 });
